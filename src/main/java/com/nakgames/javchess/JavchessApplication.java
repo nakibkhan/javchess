3815da13899bce7eb5f3b207db7964194f192338
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-06-24 16:52:08 -0500

    (add) shell application

---
+++ src/main/java/com/nakgames/javchess/JavchessApplication.java
@@ -0,0 +1,13 @@
+package com.nakgames.javchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakgames/javchess/JavchessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakgames.javchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
